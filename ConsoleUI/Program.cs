ffc635a6c77e9808fec481f6f71d9e6afe096b1d
Muhammed <turkmenrifat97@gmail.com>
2022-07-07 17:17:37 +0300

    7temmuz category eklentisi tamamlandı

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -14,12 +14,22 @@ namespace ConsoleUI
         //D
         static void Main(string[] args)
         {
+            //ProductTest();
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             //ProductManager productManager = new ProductManager(new InMemoryProductDal());//inmemorydata
             ProductManager productManager = new ProductManager(new EfProductDal());//entity data
 
             //foreach (var product in productManager.GetAll())
             //foreach (var product in productManager.GetAllByCategotyId(2))
-            foreach (var product in productManager.GetAllByUnitPrice(90,230))
+            foreach (var product in productManager.GetAllByUnitPrice(90, 230))
             {
                 Console.WriteLine(product.ProductName);
             }
