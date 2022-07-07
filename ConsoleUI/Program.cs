3e0e08eee0d155d70f39ba0237e5650bf9dbbb9a
Muhammed <turkmenrifat97@gmail.com>
2022-07-07 17:43:34 +0300

    7 temmuz ders sonu(ProductDTO eklendi)

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -14,7 +14,13 @@ namespace ConsoleUI
         //D
         static void Main(string[] args)
         {
-            //ProductTest();
+            ProductTest();
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
             foreach (var category in categoryManager.GetAll())
             {
@@ -29,9 +35,9 @@ namespace ConsoleUI
 
             //foreach (var product in productManager.GetAll())
             //foreach (var product in productManager.GetAllByCategotyId(2))
-            foreach (var product in productManager.GetAllByUnitPrice(90, 230))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + " / " + product.CategoryName);
             }
         }
     }
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,6 +1,7 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,10 +11,24 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal//IPRODUCTDAL'a neden ihtiyaç var?
-        //çünkü biz sadece efrb içindeki imzaları kullanmayacağız, product'a özel imzaları IProductDal içerisinde tutacağız
-        //DTO'lar orada kullanılacak, ve biz sadece EF'ye bağımlı değiliz
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
+//IPRODUCTDAL'a neden ihtiyaç var?
+//çünkü biz sadece efrb içindeki imzaları kullanmayacağız, product'a özel imzaları IProductDal içerisinde tutacağız
+//DTO'lar orada kullanılacak, ve biz sadece EF'ye bağımlı değiliz
     {
-        
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDto { 
+                                 ProductId = p.ProductId, ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName, UnitsInStock = p.UnitsInStock 
+                             };
+                return result.ToList();
+            }
+        }
     }
 }
