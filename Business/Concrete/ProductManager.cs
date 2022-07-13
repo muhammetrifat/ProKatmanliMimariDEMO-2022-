d2a8eee92290dac035b79a104dfc8eb430a471c0
Muhammed <turkmenrifat97@gmail.com>
2022-07-13 16:59:33 +0300

    11.gün

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -39,7 +39,7 @@ namespace Business.Concrete
         public IDataResult<List<Product>> GetAll()
         {
             //iş kodları
-            if (DateTime.Now.Hour == 9)//1 saat boyunca listeleme işlemini kapatıyoruz
+            if (DateTime.Now.Hour == 15)//1 saat boyunca listeleme işlemini kapatıyoruz
             {
                 return new ErrorDataResult<List<Product>>(Messages.MeintananceTime);
             }
